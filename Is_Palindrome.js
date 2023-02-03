3c9068018dd0bfcc4d3ed5007086772e23e62387
Christopher Estevez <CodingWithCDJE@gmail.com>
2023-02-03 12:37:17 -0500

    Optimized my old solution and improved space complexity from old solution

---
+++ Is_Palindrome.js
@@ -0,0 +1,18 @@
+const isPalindrome = function (x) {
+  let str = x.toString();
+  for (let i = 0; i < x.length; i++) {
+    if (str.charAt(i) === '-') return false;
+  }
+
+  let endIndex = str.length - 1;
+  for (let t = 0; t < str.length; t++) {
+    if (str.charAt(t) !== str.charAt(endIndex)) return false;
+    endIndex--;
+  }
+
+  return true;
+};
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
