c76e41b4f31a2d0c9b8cfb09246f5f6974cd8bfc
Christopher Estevez <CodingWithCDJE@gmail.com>
2023-02-03 14:25:46 -0500

    Added examples onto the file

--- Search_2D_Matrix.js
+++ Search_2D_Matrix.js
@@ -30,3 +30,24 @@ const searchMatrix = function (matrix, target) {
 
   return false;
 };
+
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    3
+  )
+);
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    13
+  )
+);
