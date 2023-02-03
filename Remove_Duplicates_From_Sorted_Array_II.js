10b529909379cbc2aec72b5d808af3de420ad91f
Christopher Estevez <CodingWithCDJE@gmail.com>
2023-02-03 14:35:20 -0500

    Added completed leetcode problem onto the repository

---
+++ Remove_Duplicates_From_Sorted_Array_II.js
@@ -0,0 +1,26 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+*/
+
+const removeDuplicates = function (nums) {
+  let counter = 0;
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[counter] !== nums[i]) {
+      counter++;
+    } else {
+      nums.splice(i, 1);
+      i = counter + 1;
+    }
+  }
+  return nums;
+};
+
+console.log(removeDuplicates([1, 1, 1, 2, 2, 3]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]));
