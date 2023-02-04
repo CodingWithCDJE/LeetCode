ee4c18c4428f696872466eafdadf6d8ac555407e
Christopher Estevez <CodingWithCDJE@gmail.com>
2023-02-04 12:13:30 -0500

    Uploading past completed problems onto github

---
+++ Min_Stack.js
@@ -0,0 +1,50 @@
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+*/
+
+const MinStack = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  let minNum = this.stack[0];
+  for (let i = 1; i <= this.stack.length; i++) {
+    if (this.stack[i] < minNum) minNum = this.stack[i];
+  }
+  return minNum;
+};
